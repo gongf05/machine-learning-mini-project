0824750c70d4d3582ecd5d894673dff15e973afe
gongf05 <fang.ucla@gmail.com>
2017-11-26 20:03:07 -0800

    update code

--- keras_first_cnn/main.py
+++ keras_first_cnn/main.py
@@ -10,15 +10,20 @@ X_train, y_train = data['features'], data['labels']
 
 # Initial Setup for Keras
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation, Flatten
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
-# TODO: Build the Fully Connected Neural Network in Keras Here
+# TODO: Build the Final Test Neural Network in Keras Here
 model = Sequential()
-
-model.add(Flatten(input_shape=(32, 32, 3)))
-model.add(Dense(units=100))
+model.add(Convolution2D(32, 3, 3, input_shape=(32, 32, 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.5))
 model.add(Activation('relu'))
-model.add(Dense(units=5))
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dense(5))
 model.add(Activation('softmax'))
 
 # preprocess data
@@ -29,5 +34,25 @@ label_binarizer = LabelBinarizer()
 y_one_hot = label_binarizer.fit_transform(y_train)
 
 model.compile('adam', 'categorical_crossentropy', ['accuracy'])
-# TODO: change the number of training epochs to 3
-history = model.fit(X_normalized, y_one_hot, nb_epoch=20, validation_split=0.2)
+history = model.fit(X_normalized, y_one_hot, nb_epoch=10, validation_split=0.2)
+
+with open('small_test_traffic.p', 'rb') as f:
+    data_test = pickle.load(f)
+
+X_test = data_test['features']
+y_test = data_test['labels']
+
+# preprocess data
+X_normalized_test = np.array(X_test / 255.0 - 0.5 )
+y_one_hot_test = label_binarizer.fit_transform(y_test)
+
+print("Testing")
+
+# TODO: Evaluate the test data in Keras Here
+metrics = model.evaluate(X_normalized_test, y_one_hot_test)
+# TODO: UNCOMMENT CODE
+for metric_i in range(len(model.metrics_names)):
+    metric_name = model.metrics_names[metric_i]
+    metric_value = metrics[metric_i]
+    print('{}: {}'.format(metric_name, metric_value))
+    
