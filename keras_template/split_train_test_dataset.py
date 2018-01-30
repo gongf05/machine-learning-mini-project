438653acc4067aed3c4263dcf6962f61ccd32d22
gongf05 <fang.ucla@gmail.com>
2018-01-29 23:03:37 -0800

    update

--- keras_template/split_train_test_dataset.py
+++ keras_template/split_train_test_dataset.py
@@ -6,25 +6,31 @@ import argparse
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def write_data(rawdir, dataset, targetdir, label_col, file_col)
+def write_data(rawdir, dataset, targetdir, label_col, file_col):
     if not os.path.exists(targetdir):
         os.makedirs(targetdir)
     for _, row in dataset.iterrows():
         file = row[file_col]
         label = row[label_col]
+        # remove folder name from file name
+        n = len(label)
+        file = file[n+1:]
+        # target file directory
+        labeldir = targetdir + '/' + str(label)
         if not os.path.exists(labeldir):
             os.makedirs(labeldir)
-        srcfile = rawdir + '/' + file
+        srcfile = rawdir + '/' + label + '/' + file
         tgtfile = labeldir + '/' + file
         shutil.copyfile(srcfile, tgtfile)
 
 
 def split_train_test_data(datadir, label):
-    rawdir = datadir + "/raw"
-    data = rawdir + "/data.csv"
-    datafile = pd.read_csv(data, spe='.', header=0)
+    rawdir = datadir + "/flower_photos"
+    data = rawdir + "/flower_photos.csv"
+    print(data)
+    datafile = pd.read_csv(data, sep=',', header=0)
     train, test = train_test_split(datafile, test_size=0.2, shuffle=True)
-    file_col = 'FileName'
+    file_col = 'Filename'
     # setup train data and directory
     write_data(rawdir, train, datadir + "/train", label, file_col)
     # setup test data and directory
